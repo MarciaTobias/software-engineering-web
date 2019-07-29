277c9eac4adb0af569286122a89ab3d0e50811f7
Marcia Tobias <marciaatobiass@gmail.com>
2019-07-29 20:57:41 +0100

    Updating index file

--- public/index.php
+++ public/index.php
@@ -1,49 +1,19 @@
 <!-- marcia-tobias student number 2985044. -->
-<!-- lucas-virgarinho student number 2989295. -->
+<!-- lucas-vigarinho student number 2989295. -->
 
 <?php
 // Connection with database
 require('../files/connect.php');
 ?>
 
-<!-- Is an instruction to the browser about what version of html the page is written in
-That declaration must be the very first thing in your HTML document, even before the HTML tag. -->
-<!DOCTYPE html>
-<!-- That tag specifies the language code english. -->
-<html lang="en">
-<!-- That element is a container for a metadata. -->
-<head>
-    <!-- Define a title for my HTML document. -->
-    <title>LanguageFluent</title>
-    <!-- Meta element is used to specify which character set is used, page description, keywords, author,
-    and other metadata. -->
-    <!-- Define the character set used. -->
-    <meta charset="utf-8">
-    <!-- Required meta tag for bootstrap (responsive). -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Load the reset file, with this is guarantee the page will run correctly at different browsers. -->
-    <link rel="stylesheet" href="../css/reset.css">
-    <!-- Load the bootstrap file, with this is possible to apply changes using the library bootstrap. -->
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <!-- Load the css file, with this is possible to apply the changes using the style file. -->
-    <link rel="stylesheet" href="../css/style.css">
-    <!-- Load the query file, with this is possible to make the site responsive. -->
-    <link rel="stylesheet" href="../css/queries.css">
-</head>
-<body>
 <!-- Include action to load header and content from main page. -->
 <?php
 include ('../views/header.php');
 include('../views/contentMainPage.php');
 ?>
+<html>
 <footer class="footer-main-page">
     <!--Include action to load the footer-->
     <?php include ('../views/footer.php') ?>
 </footer>
-</body>
 </html>
-
